0ca42410c258fcaeb0dea06e0a2a22b73582205a
Gulzhub <gulzarshariff341@gmail.com>
2023-03-05 17:25:54 +0530

    files added

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # book-library
- This is a simple library site to add and remove books that you have read or want to read.
+ This is a simple site to add and remove books that you have read or want to read.
+
+## Table of contents
+
+- [Overview](#overview)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#text-me)
+
+
+## Overview
+Add any book you like and the details would be saved in your local storage for your future reference.
+
+p.s. I was just trying out the local storage methods for the first time ;)
+
+## Links
+- Live Site URL: [Live](https://gulzhub.github.io/book-library/)
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS Grid
+- Vanilla Javascript
+
+## Text me
+- Twitter - [@gulz4you](https://twitter.com/gulz4you)
+- LinkedIn - [Gulzar Shariff](https://www.linkedin.com/in/gulzarshariff/)
---
+++ app.js
@@ -0,0 +1,176 @@
+let myLibrary = [
+  //   {
+  //     title: "Atomic Habits",
+  //     author: "James Clear",
+  //     pages: 234,
+  //     read: false,
+  //   },
+  //   {
+  //     title: "The subtle art of not giving a f*ck",
+  //     author: "Mark Manson",
+  //     pages: 200,
+  //     read: true,
+  //   },
+];
+
+window.addEventListener("DOMContentLoaded", restoreLocal);
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read}`;
+  };
+}
+
+function isInLibrary(bookTitle) {
+  return myLibrary.find((item) => item.title === bookTitle);
+}
+
+function getBook(title) {
+  return myLibrary.find((item) => item.title === title);
+}
+
+const modalBtn = document.querySelector(".modal-btn");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".modal-overlay");
+const addBookForm = document.querySelector("#addBookForm");
+const bookGrid = document.querySelector(".book-grid");
+
+modalBtn.addEventListener("click", function () {
+  addBookForm.reset();
+  modal.classList.add("active");
+  overlay.classList.add("active");
+});
+
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+const handleKeyboardInput = (e) => {
+  if (e.key === "Escape") closeModal();
+};
+
+overlay.onclick = closeModal;
+window.onkeydown = handleKeyboardInput;
+
+const updateBookGrid = () => {
+  resetBookGrid();
+  myLibrary.map((book) => {
+    createBookCard(book);
+  });
+};
+
+const resetBookGrid = () => {
+  bookGrid.innerHTML = "";
+};
+
+const createBookCard = (book) => {
+  const bookCard = document.createElement("div");
+  const title = document.createElement("p");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const buttonGroup = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const removeBtn = document.createElement("button");
+
+  bookCard.classList.add("card");
+  buttonGroup.classList.add("btn-grp");
+  readBtn.classList.add("btn");
+  removeBtn.classList.add("btn");
+  readBtn.onclick = toggleRead;
+  removeBtn.onclick = removeBook;
+
+  title.textContent = `"${book.title}"`;
+  author.textContent = book.author;
+  pages.textContent = `${book.pages} pages`;
+  removeBtn.textContent = "Remove";
+
+  if (book.read) {
+    readBtn.textContent = "Read";
+    readBtn.classList.add("read");
+  } else {
+    readBtn.textContent = "Not read";
+    readBtn.classList.add("not-read");
+  }
+
+  bookCard.appendChild(title);
+  bookCard.appendChild(author);
+  bookCard.appendChild(pages);
+  buttonGroup.appendChild(readBtn);
+  buttonGroup.appendChild(removeBtn);
+  bookCard.appendChild(buttonGroup);
+  bookGrid.appendChild(bookCard);
+};
+
+const getBookFromInput = () => {
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const isRead = document.getElementById("isRead").checked;
+  return new Book(title, author, pages, isRead);
+};
+
+const addBook = (e) => {
+  e.preventDefault();
+  const newBook = getBookFromInput();
+  if (isInLibrary(newBook.title)) {
+    alert("This book already exists in your library");
+    //put an error message here in the future, gulz ;)
+    return;
+  } else {
+    myLibrary.push(newBook);
+    saveLocal();
+    updateBookGrid();
+  }
+
+  closeModal();
+};
+
+const removeBook = (e) => {
+  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+    '"',
+    ""
+  );
+
+  myLibrary = myLibrary.filter((book) => book.title !== title);
+  saveLocal();
+  updateBookGrid();
+};
+
+const toggleRead = (e) => {
+  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+    '"',
+    ""
+  );
+  const book = getBook(title);
+
+  book.read = !book.read;
+  saveLocal();
+  updateBookGrid();
+};
+
+addBookForm.onsubmit = addBook;
+
+window.addEventListener("DOMContentLoaded", updateBookGrid);
+
+// ---LOCAL STORAGE---
+
+function saveLocal() {
+  localStorage.setItem("library", JSON.stringify(myLibrary));
+}
+
+function restoreLocal() {
+  const books = JSON.parse(localStorage.getItem("library"));
+  if (books) {
+    myLibrary = books.map((book) => {
+      return new Book(book.title, book.author, book.pages, book.read);
+    });
+  } else {
+    myLibrary = [];
+  }
+}
